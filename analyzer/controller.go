9fc80f1bdfde101de45ba8373e2ad87b74f291f7
David Calle Daza <dcalled1@eafit.edu.co>
2019-11-10 16:57:01 -0500

    a

--- analyzer/controller.go
+++ analyzer/controller.go
@@ -28,22 +28,24 @@ func initSolution(sender *gowd.Element, event *gowd.EventElement) {
 
 func solveSingleEquation() {
 	f := em["single_problem_def"].GetValue()
-	_ := svanalyzer.SetF(f)
+	err := svanalyzer.SetF(f)
+	if err != nil {
+		return
+	}
 	switch em["single_method_selector"].GetValue() {
 	case "bisection":
 		a, _ := strconv.ParseFloat(em["x1"].GetValue(), 64)
 		b, _ := strconv.ParseFloat(em["x2"].GetValue(), 64)
 		tol, _ := strconv.ParseFloat(em["tol"].GetValue(), 64)
 		maxit, _ := strconv.ParseInt(em["it"].GetValue(), 10, 32)
-		_ := svanalyzer.Bisection(a, b, tol, uint(math.Abs(float64(maxit))))
-
+		svanalyzer.Bisection(a, b, tol, uint(math.Abs(float64(maxit))))
 
 	case "false_pos":
 		a, _ := strconv.ParseFloat(em["x1"].GetValue(), 64)
 		b, _ := strconv.ParseFloat(em["x2"].GetValue(), 64)
 		tol, _ := strconv.ParseFloat(em["tol"].GetValue(), 64)
 		maxit, _ := strconv.ParseInt(em["it"].GetValue(), 10, 32)
-		_ := svanalyzer.FalsePosition(a, b, tol, uint(math.Abs(float64(maxit))))
+		svanalyzer.FalsePosition(a, b, tol, uint(math.Abs(float64(maxit))))
 	case "secant":
 
 	case "search":
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/CalculaThor/CalculaThor
 
-require github.com/CalculaThor/CalculaThor/gui
-
 go 1.13
+
+require (
+	github.com/CalculaThor/CalculaThor/analyzer v0.0.0-20191110215117-1b851d185092 // indirect
+	github.com/CalculaThor/CalculaThor/gui v0.0.0-20191110215117-1b851d185092 // indirect
+)
